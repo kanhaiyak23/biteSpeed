dd22ba63f315fc807c39de4800a46521b7c0b78f
Kanhaiya Kumar <148460812+kanhaiyak23@users.noreply.github.com>
2024-11-24 11:40:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const IdentityController = require('./controllers/identifyControllers') ;
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
